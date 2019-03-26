91631730e39b7bebae41eb204676888c52795cf2
Narayanan Ts <tsnarayanan7@gmail.com>
2019-03-26 12:15:11 +0530

    alignment

--- 4 Docker compose with multiple local containers/visits/app.js
+++ 4 Docker compose with multiple local containers/visits/app.js
@@ -1,19 +1,19 @@
-const express = require('express'),
-    redis = require('redis'),
-    app = express(),
-    client = redis.createClient({
-        host: 'redis-server',
-        port: 6379
-    });
-client.set('visits', 0);
+const express = require("express"),
+  redis = require("redis"),
+  app = express(),
+  client = redis.createClient({
+    host: "redis-server",
+    port: 6379
+  });
+client.set("visits", 0);
 
-app.get('/', (req, res) => {
-    client.get('visits', (err, visits) => {
-        res.send(`No of visits is ${visits}`);
-        client.set('visits', parseInt(visits) + 1);
-    });
+app.get("/", (req, res) => {
+  client.get("visits", (err, visits) => {
+    res.send(`No of visits is ${visits}`);
+    client.set("visits", parseInt(visits) + 1);
+  });
 });
 
 app.listen(8081, (req, res) => {
-    console.log('Listening on port 8081');
+  console.log("Listening on port 8081");
 });
